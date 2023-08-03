990b958251ca5871880701edf6aa820402348f1d
Ayubxon200811 <ayubxonrahimov20080811@gmail.com>
2023-08-03 14:32:06 +0500

    test commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import TelegramBot from "node-telegram-bot-api"
 
-const API = ""
+const API = "nsandausdnu"
 
 const bot = new TelegramBot(API, {polling:true})
 
 console.log(bot);
+console.log(API);
