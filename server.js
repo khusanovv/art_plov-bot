a231b5042db54d689b1e801a9f35aa8fb6198ed2
Ayubxon200811 <ayubxonrahimov20080811@gmail.com>
2023-08-05 12:16:38 +0500

    some thing

--- server.js
+++ server.js
@@ -15,44 +15,18 @@ Keling, avvaliga xizmat ko’rsatish tilini tanlab olaylik.`, Markup.keyboard(
         ]
     ).resize())
 })
-bot.on('contact', async(ctx) =>{
-        if (bot_langue == false ) {
-            console.log(ctx.message);
-            ctx.reply("Iltimos F.I.SH kiriting.")
-            user_name = true
-        }
-        else if(bot_langue == true){
-            ctx.reply("Пожалуйста, введите ФИО")
-            user_name = true
-        }
-})
+
 bot.on('message',(ctx) =>{
 if (ctx.message.text ==`O’zbekcha`) {
         bot_langue = false
-        ctx.reply(
-`📞 Ro'yxatdan o'tish uchun telefon raqamingizni kiriting. 
 
-Raqamni +998********* shaklida yuboring.`,
-{
-    ...Markup.keyboard([
-      Markup.button.contactRequest(" ☎️ Send Contact"),
-    ]).resize(),
-  })
 }
 else if (ctx.message.text ==`Русский`) {
     bot_langue = true
-    ctx.reply(
-`Введите свой номер телефона
 
-Отправить номер с помощью кнопки ОТПРАВИТЬ КОНТАКТ`,
-    {
-      ...Markup.keyboard([
-        Markup.button.contactRequest(" ☎️ Send Contact"),
-      ]).resize(),
-    })
 }
-
 })
+
 
 bot.launch();
 
